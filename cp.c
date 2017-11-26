8165bdfb1d8446ec28cd51cf8fb19cc861f75d92
raulfm94 <raulfm94@gmail.com>
2017-11-26 02:00:07 -0600

    Lab 12 Raul Flores

---
+++ cp.c
@@ -0,0 +1,30 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+char buf[512];
+
+int
+main(int argc, char *argv[])
+{
+  int fdOriginal;
+  int fdNew;
+  int n;
+  if (argc != 3)
+  {
+    printf(2, "Usage cp original neew\n");
+    return 1;
+  }
+  fdOriginal = open(argv[1],O_RDONLY);
+  fdNew = open(argv[2],O_CREATE |O_WRONLY);
+
+  while((n = read(fdOriginal, buf, sizeof(buf))) > 0){
+    write(fdNew, buf, n);
+  }
+
+  close(fdOriginal);
+  close(fdNew);
+
+  exit();
+}
